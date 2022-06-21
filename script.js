c7c1c488e2e12f27c9ad5555e021b6cedbb40b48
Olokoburnfire <95130156+Olokoburnfire@users.noreply.github.com>
2022-06-21 22:54:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//The solution to  Introduction to Javascript task by zuri team
+
+//name
+let name = "Olokodana AbdulKabir";
+//height
+const height = "5ft 11inch";
+// Country
+let country = "Nigeria";
+
+//output
+console.log("My name is", name +".",  "\nI\'m", height, "tall",  "and I am a citizen of ", country + ".");
